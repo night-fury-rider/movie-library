274119046f72b177c3ca078f47bb4e7d1bcde6e5
Yuvraj Patil <5191208+night-fury-rider@users.noreply.github.com>
2025-08-19 07:30:12 +0530

    Renamed the language filter.

--- app/lib/strings.constants.ts
+++ app/lib/strings.constants.ts
@@ -54,7 +54,7 @@ const DASHBOARD = {
         "Telugu",
       ],
       primaryTitle: "All",
-      title: "Audio Languages",
+      title: "Audio Language",
     },
     moreFilters: {
       expandMore: {
