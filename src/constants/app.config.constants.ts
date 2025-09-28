8530390d7cff005565f852a4e9e219634851e03c
Yuvraj Patil <5191208+night-fury-rider@users.noreply.github.com>
2025-09-28 19:58:19 +0530

    chore (settings): Added standard configs.

--- src/constants/app.config.constants.ts
+++ src/constants/app.config.constants.ts
@@ -6,10 +6,46 @@ const APP_CONFIG = {
     height: "200",
     width: 200,
   },
+  dateFormats: [
+    {
+      title: "DD-MMM-YYYY",
+      value: "DD-MMM-YYYY",
+    },
+    {
+      title: "YYYY-MM-DD",
+      value: "YYYY-MM-DD",
+    },
+  ],
+  decimalPlaces: 2,
+  font: {
+    family: "IBM Plex Serif",
+  },
+  languages: [
+    {
+      title: "English",
+      value: "en",
+    },
+  ],
+  numberFormats: [
+    {
+      title: "Indian Format (en-IN)",
+      value: "en-IN",
+    },
+    {
+      title: "US Format (en-US)",
+      value: "en-US",
+    },
+  ],
   ratings: {
     primaryValue: 0,
     imdbRatings: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
     customRatings: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+  },
+  sessionStorage: {
+    storageAppData: "movieData",
+    storageDateFormat: "dateFormat",
+    storageLanguage: "language",
+    storageNumberFormat: "numberFormat",
   },
 };
 
